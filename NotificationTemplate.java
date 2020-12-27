c8b07283f4d3dad3dcf2c9d1c636f82aaf3a9d96
Noura-Saad123 <63734618+Noura-Saad123@users.noreply.github.com>
2020-12-27 16:44:13 +0200

    Add files via upload

---
+++ NotificationTemplate.java
@@ -0,0 +1,11 @@
+package com.example.demo.core;
+
+
+public class NotificationTemplate {
+    public int id;
+    public String subject;
+    public String content;
+    public languageEnum language;
+
+
+}
---
+++ notification.java
@@ -0,0 +1,11 @@
+package com.example.demo.core;
+
+public class notification {
+    public String sendType;
+    public  String subject;
+    public  String content;
+    public  String placeholder;
+    public  String email;
+    public  String number;
+    public int tempID;
+}
